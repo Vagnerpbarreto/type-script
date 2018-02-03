791dc23985e9372e21c5c8331ea6458f483790e0
Vagner Barreto <vagnerpbarreto@gmail.com>
2018-02-03 16:13:02 -0200

    This is a new commit for what I originally planned to be amended

---
+++ app.ts
@@ -0,0 +1,9 @@
+let message: string = "Hello World!!!!!";
+console.log(message);
+
+let episode:number = 4;
+console.log("This is episode " + episode);
+
+episode = episode + 1;
+
+console.log("Next Episode is " + episode);
